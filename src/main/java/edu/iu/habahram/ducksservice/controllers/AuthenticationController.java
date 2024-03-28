b5947bdd077bbad0a9592813f464d97119f86483
brilord <143294125+hellobrian1@users.noreply.github.com>
2024-03-28 12:54:14 -0400

    committing

--- src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
+++ src/main/java/edu/iu/habahram/ducksservice/controllers/AuthenticationController.java
@@ -1,2 +1,30 @@
-package edu.iu.habahram.ducksservice.controllers;public class AuthenticationController {
+package edu.iu.habahram.ducksservice.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.iu.habahram.ducksservice.model.Customer;
+import edu.iu.habahram.ducksservice.model.CustomerRepository;
+
+@RestController
+@CrossOrigin
+public class AuthenticationController {
+
+    CustomerRepository customerRepository;
+
+    public AuthenticationController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody Customer customer) {
+        try {
+            customerRepository.save(customer);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
+
--- src/main/java/edu/iu/habahram/ducksservice/model/Costomer.java
+++
@@ -1,2 +0,0 @@
-package edu.iu.habahram.ducksservice.model;public class Costomer {
-}
